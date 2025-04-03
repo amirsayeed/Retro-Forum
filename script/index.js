460277f7aa6c0a0aa98b1cb4ca0be52bafb7acb6
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2025-04-03 20:45:30 +0600

    latest posts section data added in card dynamically

--- script/index.js
+++ script/index.js
@@ -4,11 +4,16 @@ const loadPosts = async () => {
     showPosts(data.posts);
 }
 
+const loadLatestPosts = async () => {
+    const response = await fetch("https://openapi.programming-hero.com/api/retro-forum/latest-posts");
+    const data = await response.json();
+    displayLatestPosts(data);
+}
+
 const showPosts = posts => {
     //console.log(posts);
     const postContainer = document.getElementById("post-container");
     posts.map(post => {
-        console.log(post)
         const div = document.createElement("div");
         div.innerHTML = `
         <div class = "flex items-center bg-[#797DFC1A] shadow-sm p-5 rounded-lg">
@@ -46,4 +51,38 @@ const showPosts = posts => {
         postContainer.appendChild(div)
     })
 }
+
+const displayLatestPosts = latests => {
+    const latestContainer = document.getElementById("latest-post-container");
+    latests.map(latest => {
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="card bg-base-100 p-6 shadow-md rounded-md">
+        <figure>
+            <img
+            src=${latest.cover_image} class="rounded-lg"
+            alt="Shoes" />
+        </figure>
+        <div class="text-left space-y-2 mt-5">
+            <p class="flex gap-3">
+                <span><i class="fa-regular fa-calendar fa-lg"></i></span>
+                <span>${latest?.author?.posted_date ? latest?.author?.posted_date : "No publish date" }</span>
+            </p>
+            <h2 class="text-lg font-medium">${latest.title}</h2>
+            <p>${latest.description}</p>
+            <div class="flex gap-2">
+            <img src = ${latest.profile_image} class="w-10 h-10 rounded-full object-cover" />
+            <div>
+            <p class="text-base font-medium">${latest?.author?.name}</p>
+            <p class="text-sm">${latest?.author?.designation ? latest?.author?.designation : "Unknown" }</p>
+            </div>
+            </div>
+        </div>
+        </div>
+        `
+        latestContainer.appendChild(div);
+    })
+}
+
+loadLatestPosts()
 loadPosts()
